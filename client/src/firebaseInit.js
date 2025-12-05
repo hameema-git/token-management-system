45955c6c921280379b9e7dc381d3f3f67f18e20b
Hameema <hameema@gmail.com>
2025-12-06 02:12:29 +0530

    Initial token system12

--- client/src/firebaseInit.js
+++ client/src/firebaseInit.js
@@ -4,15 +4,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBWCLhBDLPkZydb_RJ3qu7uOMoHCXhWtNE",
-  authDomain: "token-management-system-v1.firebaseapp.com",
-  projectId: "token-management-system-v1",
-  storageBucket: "token-management-system-v1.firebasestorage.app",
-  messagingSenderId: "975705895758",
-  appId: "1:975705895758:web:ecab2dac38600b7dad641f",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-
-// Firestore instance
 export const db = getFirestore(app);
